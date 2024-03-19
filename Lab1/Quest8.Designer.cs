fbfec637e6cecd4ac0e33233b8af6e5f635b270d
KoiH <22520702@gm.uit.edu.vn>
2024-03-19 09:24:09 +0700

    Update !

---
+++ Lab1/Quest8.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace LTMCB_Lab1
+{
+    partial class Quest8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.lbInput = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.tbxTemp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(89, 10);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(293, 20);
+            this.tbxInput.TabIndex = 0;
+            this.tbxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxInput_KeyDown);
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(13, 13);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(70, 13);
+            this.lbInput.TabIndex = 1;
+            this.lbInput.Text = "Nhập dữ liệu:";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(16, 37);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(366, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Xử lý dữ liệu";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            // 
+            // tbxTemp
+            // 
+            this.tbxTemp.Location = new System.Drawing.Point(16, 66);
+            this.tbxTemp.Multiline = true;
+            this.tbxTemp.Name = "tbxTemp";
+            this.tbxTemp.ReadOnly = true;
+            this.tbxTemp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxTemp.Size = new System.Drawing.Size(366, 211);
+            this.tbxTemp.TabIndex = 99;
+            this.tbxTemp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxInput_KeyDown);
+            // 
+            // Quest8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 289);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lbInput);
+            this.Controls.Add(this.tbxTemp);
+            this.Controls.Add(this.tbxInput);
+            this.Name = "Quest8";
+            this.Text = "Quest8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox tbxTemp;
+    }
+}
---
+++ Lab1/Quest8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTMCB_Lab1
+{
+    public partial class Quest8 : Form
+    {
+        public Quest8()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void tbxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+            }
+        }
+    }
+}
