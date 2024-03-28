528bc3bfa3bcc583bdf37a703d8f07cd5d7a1608
KoiH <22520702@gm.uit.edu.vn>
2024-03-28 20:59:00 +0700

    Update Form1, Form2

--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Lab2
 {
@@ -15,6 +16,30 @@ namespace Lab2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string text = sr.ReadToEnd();
+            tbxShow.Text = text;
+            sr.Close();
+            fs.Close();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            FileStream fs = new FileStream(ofd.FileName, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            string text = tbxShow.Text.ToUpper();
+            sw.Write(text);
+            sw.Close();
+            fs.Close();
         }
     }
 }
